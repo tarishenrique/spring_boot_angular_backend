919d27ff66713a570b1e6c61c317aad175120dea
Taris Henrique <taris.tj@hotmail.com>
2023-04-17 00:39:59 -0300

    Chapter 4: Creating JPA Entity and JPA Repository video 24m19s

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,5 +1,61 @@
 package net.javaguides.springboot.model;
 
-public class Employee {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "emplyees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email_id")
+	private String emailid;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailid) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailid = emailid;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
 }
