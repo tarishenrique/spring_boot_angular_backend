8ac6439f358b5afd37d53bd1a2dd69d04f6a5d69
Taris Henrique <taris.tj@hotmail.com>
2023-04-17 11:20:42 -0300

    feature: criada lista de empregados utilizando restapi video 32m12s

---
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// recebe todos os empregados
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmplyees(){
+		return employeeRepository.findAll()
+;	}
+	
+
+}
--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "emplyees")
+@Table(name = "employees")
 public class Employee {
 	
 	@Id
