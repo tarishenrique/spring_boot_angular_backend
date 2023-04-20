4fca31e96f30cb838095d2784ad3ff9f488263f7
Taris Henrique <taris.tj@hotmail.com>
2023-04-19 23:25:35 -0300

    alterada opcao no controller para liberar conexao do frontend

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -18,6 +20,7 @@ public class EmployeeController {
 	private EmployeeRepository employeeRepository;
 	
 	// recebe todos os empregados
+	
 	
 	@GetMapping("/employees")
 	public List<Employee> getAllEmplyees(){
