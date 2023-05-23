6e566f2934c32e446815263bf3c6922b0077ebca
Taris Henrique <taris.tj@hotmail.com>
2023-05-22 23:08:38 -0300

    API deletar usuario

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package net.javaguides.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +62,19 @@ public class EmployeeController {
 		Employee empregadoAtualizado = employeeRepository.save(empregado);
 		
 		return ResponseEntity.ok(empregadoAtualizado);
+		
+	}
+	
+	// API deletar empregados
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee empregado = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+				"Não existe registro de empregado com id = " + id));
+		
+		employeeRepository.delete(empregado);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 		
 	}
 	
